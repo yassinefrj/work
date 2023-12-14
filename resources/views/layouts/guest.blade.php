8fdda7d96c873c89b44a64872114daefa973af6f
yassinefrj <147695883+yassinefrj@users.noreply.github.com>
2023-12-14 14:41:40 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         
-        @vite(['resources/css/app.css', 'resources/js/app.js
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     </head>
     <body class="font-sans text-gray-900 antialiased">
