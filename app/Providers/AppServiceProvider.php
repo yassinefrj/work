6a3a97a1a18d7ee405d6316f0dc419fa5699d8dd
yassinefrj <147695883+yassinefrj@users.noreply.github.com>
2023-12-14 14:05:31 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+       // if($this->app->environment('production')) {
+         //   URL::forceScheme('https');
+        //}
     }
 }
