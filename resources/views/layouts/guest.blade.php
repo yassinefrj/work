37cb6247fa6624b95e0320afab6cf40ed37fe995
yassinefrj <147695883+yassinefrj@users.noreply.github.com>
2023-12-14 14:34:35 +0100

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,11 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts 
+        
         @vite(['resources/css/app.css', 'resources/js/app.js
-        -->
-        <link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
-        <script type="module" src="{{ asset('resources/js/app.js') }}"></script>
 
     </head>
     <body class="font-sans text-gray-900 antialiased">
